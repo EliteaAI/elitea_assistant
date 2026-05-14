0f73291dd6b0d9b688134b975ce20f1dbeb03ad1
Maks Shamanskii <m.shamanskyi91@gmail.com>
2026-05-14 12:16:25 +0300

    fix: [EL-4904] Fix streaming chunk duplications and UI issues

--- src/components/icons/index.ts
+++ src/components/icons/index.ts
@@ -3,6 +3,7 @@ export { default as AttachmentIcon } from './AttachmentIcon';
 export { default as ChatIcon } from './ChatIcon';
 export { default as CheckIcon } from './CheckIcon';
 export { default as CloseIcon } from './CloseIcon';
+export { default as CollapseIcon } from './CollapseIcon';
 export { default as CopyIcon } from './CopyIcon';
 export { default as ErrorIcon } from './ErrorIcon';
 export { default as ExpandIcon } from './ExpandIcon';
